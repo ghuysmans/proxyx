02aa4e5e4719c6b44fe13f50b8b0ae1dee0a10ed
Guillaume Huysmans <ghuysmans99@gmail.com>
2015-04-02 01:56:47 +0200

    Refactored the client POC
    - Created get_tcp_socket from get_tcp_client
    - Added the include directory to the path

--- poc/client.c
+++ poc/client.c
@@ -1,8 +1,8 @@
+#include "socket.h"
 #include <unistd.h>
 #include <string.h>
 #include <error.h>
 #include <errno.h>
-#include <netdb.h>
 #include <sys/types.h>
 #include <sys/socket.h>
 #include <stdio.h>
@@ -11,46 +11,10 @@
 	"Connection: close\r\n" \
 	"Host: www.google.fr\r\n\r\n"
 
-int getTcpClient(char *host, char *service) {
-	int sock=-1, e;
-	const struct addrinfo hints = {0, AF_UNSPEC, SOCK_STREAM,
-		0, 0, NULL, NULL, NULL};
-	struct addrinfo *res=NULL, *p;
-	if (e = getaddrinfo(host, service, &hints, &res))
-		fprintf(stderr, "getaddrinfo: %s\n", gai_strerror(e));
-	for (p=res; p; p=p->ai_next) {
-		char ip[40];
-		int h = getnameinfo(p->ai_addr, p->ai_addrlen, ip, sizeof(ip),
-				NULL, 0, NI_NUMERICHOST);
-		if (h) {
-			fprintf(stderr, "getnameinfo: %s\n", gai_strerror(h));
-			*ip = 0; //not fatal
-		}
-		if ((sock=socket(p->ai_family, p->ai_socktype, p->ai_protocol)) == -1) {
-			perror("socket");
-			continue;
-		}
-		fprintf(stderr, "trying %s... ", *ip ? ip : "(?)");
-		if (connect(sock, p->ai_addr, p->ai_addrlen) == -1) {
-			perror("connect");
-			close(sock);
-			sock = -1;
-		}
-		else
-			break;
-	}
-	if (sock == -1)
-		fprintf(stderr, "couldn't connect to %s\n", host);
-	else {
-		fprintf(stderr, "connected\n");
-		freeaddrinfo(res);
-	}
-	return sock;
-}
-
 int main(int argc, char *argv[]) {
 	char *prot="http";
 	int o, verbose=0;
+	//TODO use a port of this library on Windows
 	while ((o=getopt(argc, argv, "p:v")) != -1) {
 		switch (o) {
 			case 'p':
@@ -65,11 +29,13 @@ int main(int argc, char *argv[]) {
 		int sock;
 		ssize_t r;
 		char buf[1024];
-		if ((sock=getTcpClient(argv[optind], prot)) == -1)
+		if ((sock=get_tcp_socket(argv[optind], prot, 0)) == -1)
 			return 1;
-		if (send(sock, EASYGET, strlen(EASYGET), 0) == -1)
-			error(errno, errno, "send");
-		while ((r = recv(sock, buf, sizeof(buf), 0))!=0 && r!=-1) {
+		if (send(sock, EASYGET, strlen(EASYGET), 0) == -1) {
+			perror("send");
+			return errno;
+		}
+		while ((r = recv(sock, buf, sizeof(buf)-1, 0))!=0 && r!=-1) {
 			buf[r] = 0;
 			puts(buf);
 		}
--- src/socket.c
+++ src/socket.c
@@ -3,11 +3,40 @@
  * Low-level sockets helpers
  */
 #include "socket.h"
+#include <unistd.h>
+#include <error.h>
+#include <errno.h>
+#include <sys/types.h>
+#include <sys/socket.h>
+#include <netdb.h>
+#include <stdio.h>
+
 
 /**
- * Creates a TCP socket.
+ * Creates a TCP client/server socket.
  * @return descriptor
  */
-int get_tcp_socket(const char *host /**<host to connect/listen to*/,
-		const char *service /**<service or port number*/,
-		int is_server /**<boolean*/);
+int get_tcp_socket(const char *host /**<host to connect to (name or IP)*/,
+		const char *service /**<service name or port number*/,
+		int is_server /**<boolean, implies host==NULL*/) {
+	int sock=-1, e;
+	const struct addrinfo hints = {is_server?AI_PASSIVE:0,
+		AF_UNSPEC, SOCK_STREAM, 0, 0, NULL, NULL, NULL};
+	struct addrinfo *res=NULL, *p;
+	if (e = getaddrinfo(host, service, &hints, &res))
+		fprintf(stderr, "getaddrinfo: %s\n", gai_strerror(e));
+	else {
+		for (p=res; p; p=p->ai_next) {
+			if ((sock=socket(p->ai_family, p->ai_socktype, p->ai_protocol)) == -1)
+				continue;
+			else if (connect(sock, p->ai_addr, p->ai_addrlen) == -1) {
+				close(sock);
+				sock = -1;
+			}
+			else
+				break;
+		}
+		freeaddrinfo(res);
+	}
+	return sock;
+}
