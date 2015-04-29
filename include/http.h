c92c35ab49ff4dbee73ebd5b14d543e1c47ccabc
Guillaume Huysmans <ghuysmans99@gmail.com>
2015-04-29 14:01:10 +0200

    Implemented a buggy fetch_http based on jae's work

--- include/http.h
+++ include/http.h
@@ -3,7 +3,7 @@
 
 #include "http_headers.h"
 
-int fetch_http(const int sockfd, char *receive, const size_t receive_len, char **start_line, HTTP_HEADER **headers, char **data, size_t *data_len);
+int fetch_http(const int sockfd, char *buf, const size_t buf_len, char **start_line, HTTP_HEADER **headers, char **data, size_t *data_len, size_t *remaining);
 void handle_http_client(int sockfd);
 
 #endif //_HTTP_H
--- src/http.c
+++ src/http.c
@@ -3,80 +3,107 @@
  * HTTP library
  */
 #include "http.h"
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
 
 /**
  * Fetches data using HTTP.
+ * @return -1 on error
  * @return 0 when data is populated
- * @return 1 when data is incomplete (too large).
+ * @return 1 when data is incomplete (too large)
  */
 int fetch_http(const int sockfd /**<socket to read the data from*/,
-		char *receive /**<receive buffer*/,
-		const size_t receive_len /**<receive buffer length*/,
+		char *buf /**<receive buffer*/,
+		const size_t buf_len /**<receive buffer length*/,
 		char **start_line /**<the line before the headers block*/,
 		HTTP_HEADER **headers /**<received headers*/,
 		char **data /**<data buffer*/,
-		size_t *data_len /**<data length*/);
-
-/**
- * Handles an HTTP client.
- */
-void handle_http_client(int sockfd /**<socket returned by accept()*/);
-
-int fetch_response(int socket, char** raw_headers, char** raw_content){
-	int nread, len;//nread seems to be the number of bytes we already read, len seems to be the length of what we got so far for the header/content
-	int headers_len=0, content_len=0;
-	char buf[BUFFER_LEN], *p;
-	//init for realloc
-	*raw_headers = NULL;
-	*raw_content = NULL;
-
-	//headers
-	//here, p is the end of the headers part
+		size_t *data_len /**<data length*/,
+		size_t *remaining /**<amount of data still on the network*/) {
+	//FIXME duplicate headers strings to free them more easily
+	int headers_len=0, nread, added_len;
+	char *raw_headers=NULL, *p;
+	char *soh /**<start of headers (the start line is skipped)*/;
+	*data = NULL;
+	*headers = NULL;
+	*data_len = 0;
 	do {
-		nread=read(socket, buf, BUFFER_LEN-1);
-		printf("Read %d bytes\n", nread);
-		if (nread == -1) {
-			free(*raw_headers);
-			error("Header reading error");
-			return 2;
+		if ((nread=read(sockfd, buf, buf_len-1)) == -1) {
+			error("fetch_http: headers");
+			goto fail;
 		}
-		if (p = strstr(buf, "\r\n\r\n")){//If we reached the end (p then is a pointer to the beggining of the string)
-			len = p-buf;//p-buf+2
-			printf("current header piece len : %d\n",len);
-		}
+		//FIXME bug when the empty line is on a boundary
+		else if (p = strstr(buf, "\r\n\r\n"))
+			//we reached the end of headers
+			added_len = p-buf+2;
 		else
-			len = nread;
-		*raw_headers = realloc(*raw_headers, headers_len+len+1);
-		if (*raw_headers == NULL)
-			return 1;
-		buf[len] = 0;//0 terminal
-		strcat(*raw_headers, buf);//Add in raw_headers what we parsed so far
-		headers_len += len;
-	} while (!p);
-
-	//content
-	do {
-		if (p){
-			len = &buf[nread]- p - 4;//Everything minus the header and \r\n\r\n
-			printf("current content piece len : %d\n",len);
+			//that's a full block
+			added_len = nread;
+		if (raw_headers = realloc(raw_headers, headers_len+added_len+1)) {
+			buf[added_len] = 0; //add a terminator
+			strcat(raw_headers, buf);
+			headers_len += added_len;
 		}
-		else {//Error with the header
-			len = read(socket, buf, BUFFER_LEN-1);
-			if (len == -1) {
-				free(*raw_headers);
-				free(*raw_content);
-				error("Reading error");
-				return 2;
+		else {
+			perror("fetch_http: realloc\n");
+			return -1;
+		}
+	} while (!p);
+	*start_line = raw_headers;
+	p += 4; //start of data
+	nread += buf-p; //remaining data
+	soh = strchr(raw_headers, '\r');
+	*soh = 0; //terminate start_line
+	if (*headers = parse_http_headers(soh+2, NULL)) {
+		char *h = find_http_header(*headers, "Content-Length");
+		int first = 1; //first while iteration
+		if (h && *h)
+			*remaining = atoi(h);
+		else if (**start_line=='1' || //FIXME no_data flag
+				((**start_line=='2' || **start_line=='3') &&
+				!strncmp(*start_line+1, "04", 2)))
+			*remaining = 0; //no data
+		else
+			*remaining = -1; //until the connection closes
+		while (*remaining) {
+			if (nread) {
+				*remaining -= nread;
+				*data_len += nread;
+				if (*data = realloc(*data, *data_len)) {
+					memcpy(*data+*data_len-nread, p, nread);
+					if (first) {
+						//we managed not to lose the first bytes of data.
+						p = buf; //first, reset the pointer used by memcpy
+						//now, let's return if it's too large.
+						//TODO detect size/type
+						first = 0; //do this only once
+					}
+					nread = 0;
+				}
+				else {
+					perror("fetch_http: realloc\n");
+					goto fail;
+				}
+			}
+			else if ((nread=read(sockfd, buf, buf_len-1)) == -1) {
+				if (*remaining == -1)
+					break; //normal termination
+				else {
+					error("fetch_http: read");
+					goto fail;
+				}
 			}
 		}
-		*raw_content = realloc(*raw_content, content_len+len+1);
-		if (*raw_content == NULL)
-			return 1;
-		//LOL NOT WORKING WITH BINARY CONTENT, U MAD BRO ???
-		//buf[nread] = 0;
-		//strcat(*raw_content, p+4);//we skip the ending '\r\n\r\n' and we start right after that
-		memcpy(*raw_content, p+4, nread-headers_len-4);
-		content_len += len;
-	} while (0);
-	return 0;
+		return 0;
+	}
+	else {
+fail:
+		if (*data) {
+			free(*data);
+			*data = NULL;
+		}
+		free(raw_headers);
+		return -1;
+	}
 }
