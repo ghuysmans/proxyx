cbc21b3862ed09b52ce08c1a1807ee1a1e9a3a0d
Guillaume Huysmans <ghuysmans99@gmail.com>
2015-04-29 14:05:15 +0200

    Improved status to display what fetch_http has done

--- poc/status.c
+++ poc/status.c
@@ -1,31 +1,36 @@
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <fcntl.h>
+
 #include <string.h>
 #include <stdio.h>
 #include <stdlib.h>
 #include <error.h>
 
+#include "http_headers.h"
+#include "http.h"
+
 int main(int argc, char *argv[]) {
-	char *s = strdup("HTTP/1.1 200 OK\r\na: b\r\nc: d\r\n");
-	char *version=NULL, *status=NULL, *message=NULL, *p;
-	int ret = 0;
-	if (p=strstr(s, "\r\n")) {
-		*p = 0;
-		p += 2;
-		if (!(version=strtok(s, "/")) || strcmp(version, "HTTP"))
-			ret = 3; //wrongly unspecified protocol
-		else if ((version=strtok(NULL, " ")) &&
-				(status=strtok(NULL, " ")) &&
-				(message=strtok(NULL, "\r"))) {
-			printf("version is %s\n", version);
-			printf("status is %s\n", status);
-			printf("message is %s\n", message);
-			printf("%s\n", p);
-		}
-		else
-			ret = 2; //invalid syntax
+	int fd = open("a.txt", O_RDONLY);
+	char buf[100], *fl, *d;
+	HTTP_HEADER *h;
+	size_t dl, rem;
+	int ret;
+
+	if (fd == -1) {
+		perror("open");
+		return 1;
 	}
-	else
-		ret = 1; //no status line
-end:
-	free(s);
-	return ret;
+	else if ((ret=fetch_http(fd, buf, sizeof(buf), &fl, &h, &d, &dl, &rem)) == -1)
+		return -1;
+	else {
+		HTTP_HEADER *p;
+		printf("headers:\n");
+		for (p=h; p; p=p->next)
+			printf("%s=%s\n", p->field_name, p->field_value);
+		printf("data:\n%s", d);
+		free_http_headers(h);
+		free(d);
+		return 0;
+	}
 }
