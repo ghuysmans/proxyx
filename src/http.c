5f3fd999bd01eac9e6a224e61c23fa7066ddc3e5
Guillaume Huysmans <ghuysmans99@gmail.com>
2015-05-02 13:59:47 +0200

    Handled errors correctly in fetch_http

--- src/http.c
+++ src/http.c
@@ -21,73 +21,71 @@ int fetch_http(const int sockfd /**<socket to read the data from*/,
 		size_t *remaining /**<remaining data length*/) {
 	//FIXME duplicate headers strings to free them more easily
 	int sll = read_until(sockfd, start_line, "\r\n", chunk, b);
-	if (sll == -1)
-		return 1;
-	else {
-		char *rh;
-		int headers_len;
-		if (sll < 2) {
+	*headers = NULL;
+	*data = NULL;
+	if (sll < 2) {
 nope:
+		if (*start_line) {
 			free(*start_line);
 			*start_line = NULL;
-			return 1;
 		}
-		else {
-			(*start_line)[sll-2] = 0; //terminator
-			headers_len = read_until(sockfd, &rh, "\r\n\r\n", chunk, b);
+		return 1;
+	}
+	else {
+		char *rh; //raw headers
+		char *cl; //Content-Length header value
+		size_t headers_len, full_length;
+		(*start_line)[sll-2] = 0; //terminator
+		headers_len = read_until(sockfd, &rh, "\r\n\r\n", chunk, b);
+		if (headers_len < 4) {
+			//no headers
+			if (rh)
+				free(rh);
 			if (headers_len == -1)
 				goto nope;
-			else {
-				//terminator
-				//FIXME does it even work?
-				if (headers_len < 2)
-					rh[headers_len] = 0;
-				else
-					rh[headers_len-2] = 0;
-				if (*headers = parse_http_headers(rh, NULL)) {
-					char *h = find_http_header(*headers, "Content-Length");
-					size_t full_length;
-					if (h && *h) {
-						full_length = atoi(h);
-						if (full_length > chunk)
-							*remaining = full_length-chunk;
-						else
-							*remaining = 0;
-						*data_len = full_length-*remaining;
-						if (*data_len) {
-							if (*data = malloc(*data_len+1)) {
-								int nread = read_buffered(sockfd, *data, *data_len, b);
-								(*data)[nread] = 0;
-								return nread != *data_len;
-							}
-							else
-								return 1;
-						}
-						else {
-							*data = NULL;
-							return 0;
-						}
-					}
-					else if (!strncmp(*start_line, "HTTP/1.1 1", 10) ||
-							!strncmp(*start_line, "HTTP/1.1 204", 12) ||
-							!strncmp(*start_line, "HTTP/1.1 304", 12)) {
-						*data_len = *remaining = 0;
-						*data = NULL;
-					}
-					else {
-						*remaining = -1; //unknown
-						if (*data = malloc(*data_len+1)) {
-							*data_len = read_buffered(sockfd, *data, chunk, b);
-							if (*data_len < chunk)
-								*remaining = 0;
-							(*data)[*data_len] = 0;
-							return 0;
-						}
-						else
-							return 1;
-					}
-				}
-			}
 		}
+		else {
+			rh[headers_len-2] = 0; //terminator
+			*headers = parse_http_headers(rh, NULL);
+		}
+		cl = find_http_header(*headers, "Content-Length");
+		if (cl) {
+			full_length = atoi(cl);
+			if (full_length > chunk)
+				*remaining = full_length-chunk;
+			else
+				*remaining = 0;
+			*data_len = full_length-*remaining;
+			if (*data_len) {
+				if (*data = malloc(*data_len+1)) {
+					int nread = read_buffered(sockfd, *data, *data_len, b);
+					(*data)[nread] = 0;
+					return nread != *data_len;
+				}
+				else
+					return 1;
+			}
+			else
+				//empty
+				*data = NULL;
+		}
+		else if (!strncmp(*start_line, "HTTP/1.1 1", 10) ||
+				!strncmp(*start_line, "HTTP/1.1 204", 12) ||
+				!strncmp(*start_line, "HTTP/1.1 304", 12)) {
+			*data_len = *remaining = 0;
+			*data = NULL;
+		}
+		else {
+			*remaining = -1; //unknown
+			if (*data = malloc(*data_len+1)) {
+				*data_len = read_buffered(sockfd, *data, chunk, b);
+				if (*data_len < chunk)
+					*remaining = 0;
+				(*data)[*data_len] = 0;
+			}
+			else
+				return 1;
+		}
+		return 0;
 	}
 }
